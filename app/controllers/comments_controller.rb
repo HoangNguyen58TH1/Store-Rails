00412a4a68f08b8aaff63384b92d02356688eeb6
HoangNguyen58TH1 <46235937+HoangNguyen58TH1@users.noreply.github.com>
2021-03-30 11:33:23 +0700

    Master (#8)
    * convert version ruby 2.6.6
    
    * update
    
    * update rm sqlite3 from gemfile.lock
    
    * Revert "update rm sqlite3 from gemfile.lock"
    
    This reverts commit f998f7c52ecdc6f5307103351fdd99bdcf7201f3.
    
    * add local time HCM
    
    * authen_basic, association & delete comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,20 @@
 class CommentsController < ApplicationController
+
+	http_basic_authenticate_with name: "hoangtoni", password: "123qwe", only: :destroy
+
 	def create
 		@article = Article.find(params[:article_id])
 		@comment = @article.comments.create(comment_params)
 		redirect_to article_path(@article)
+	end
+
+	def destroy
+		# @article = Article.find(params[:article_id])
+		@comment = Comment.find(params[:id])
+		@article = @comment.article
+		@comment.destroy
+
+		redirect_to @article
 	end
 
 	private
